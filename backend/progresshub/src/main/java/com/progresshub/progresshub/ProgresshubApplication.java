e8fea8e9db46f7745f659dd16bbdc84411892219
Amisha103 <bhatiaamisha74@gmail.com>
2026-08-03 11:15:13 +0530

    Initial project setup

---
+++ backend/progresshub/src/main/java/com/progresshub/progresshub/ProgresshubApplication.java
@@ -0,0 +1,13 @@
+package com.progresshub.progresshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgresshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgresshubApplication.class, args);
+	}
+
+}
---
+++ backend/progresshub/src/test/java/com/progresshub/progresshub/ProgresshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresshub.progresshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgresshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
